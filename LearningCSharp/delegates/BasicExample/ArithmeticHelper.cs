9a823150e60d07729a2b2a2d3bfe9c278ce223d4
Sanjeev <64134945+sanjeev9160@users.noreply.github.com>
2022-12-15 12:17:35 +0530

    Update ArithmeticHelper.cs

--- LearningCSharp/delegates/BasicExample/ArithmeticHelper.cs
+++ LearningCSharp/delegates/BasicExample/ArithmeticHelper.cs
@@ -11,8 +11,9 @@ namespace LearningCSharp.delegates.BasicExample
             return operation(x, y); // same as: operation?.Invoke(x, y);
         }
 
-        // Action - void return type
-        // Func - 
+        // 2 main delegates
+        // Action - void return type. They are predefined and can be directly used...:)
+        // Func - have a return type. They are predefined and can be directly used...:)
         public static int OperationWithFunc(Func<int, int, int> operation, int x, int y)
         {
             return operation(x, y); // same as: operation?.Invoke(x, y);
